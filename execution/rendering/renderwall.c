f364ee653b71c1ebc012dc962162a863848e8b91
alaalalm <achrfalalix@gmail.com>
2024-05-31 19:59:13 +0100

    ceiling and floor are colored

--- execution/rendering/renderwall.c
+++ execution/rendering/renderwall.c
@@ -6,12 +6,39 @@
 /*   By: alaalalm <alaalalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 17:06:33 by alaalalm          #+#    #+#             */
-/*   Updated: 2024/05/31 19:18:14 by alaalalm         ###   ########.fr       */
+/*   Updated: 2024/05/31 19:58:27 by alaalalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execution.h"
 
+void renderCeiling(t_data *data, int rayId, int y, int color)
+{
+    int i;
+    int j;
+
+    i = -1;
+    while (++i < y)
+    {
+        j = -1;
+        while (++j < 1)
+            *(unsigned int *)(data->addr + ((i) * data->line_length + (rayId) * (data->bits_per_pixel / 8)) ) = color;
+    }
+}
+
+void renderFloor(t_data *data, int rayId, int y, int color)
+{
+    int i;
+    int j;
+
+    i = -1;
+    while (++i < data->W_Height - y)
+    {
+        j = -1;
+        while (++j < 1)
+            *(unsigned int *)(data->addr + ((y + i) * data->line_length + (rayId) * (data->bits_per_pixel / 8)) ) = color;
+    }
+}
 void renderWall(t_data *data, int rayId, int y, int wallHeight, int color)
 {
     int i;
@@ -41,8 +68,10 @@ void render_column(t_data *data, int rayId, double rayAngle, double distance)
     if (wallHeight < 0)
         wallHeight = 0;
     if (data->rays->wasHitVertical)
-        color = 0x00FF0000;
+        color = 0xa8a0a0;
     else
-        color = 0x0000FF00;
+        color = 0xf5ebeb;
     renderWall(data, rayId, (data->W_Height / 2) - (wallHeight / 2), wallHeight, color);
+    renderCeiling(data, rayId, (data->W_Height / 2) - (wallHeight / 2), 0xADD8E6);
+    renderFloor(data, rayId, (data->W_Height / 2) + (wallHeight / 2), 0xdb4d4d);
 }   
